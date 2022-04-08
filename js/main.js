d6793be9a361ad48f99a93165a1e021d3898f769
metlx <metlx>
2022-04-08 16:20:02 -0400

    small refactor

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
-
-function getFetch(){
+document.querySelector('button').addEventListener('click', function getFetch(){
   const url = 'https://api.nasa.gov/planetary/apod?count=1&api_key=A9ahsEHwrHfKjzzeRs1Lk4cmubv8nMsRPiWMADY0'
-  // field1=value1&field2=value2&field3=value3...
   fetch(url)
-    .then(res => res.json()) // parse response as JSON
+    .then(res => res.json())
     .then(data => {
       document.getElementById('h2').innerText = data[0].title
       document.body.background = data[0].url
@@ -13,4 +9,4 @@ function getFetch(){
     .catch(err => {
       console.log(`error ${err}`)
     });
-}
+})
